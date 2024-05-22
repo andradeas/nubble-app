9813748f18707d5cea50cce7b03abbba9d815a85
André Andrade Santos <whoisandrade@gmail.com>
2024-05-21 23:52:28 -0300

    add textInput component

--- App.tsx
+++ App.tsx
@@ -3,7 +3,8 @@ import {SafeAreaView, View} from 'react-native';
 import {Text} from './src/components/Text/Text';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
-import {Box} from './src/components/Box/Box';
+import {Button} from './src/components/Button/Button';
+import {TextInput} from './src/components/TextInput/TextInput';
 import {Icon} from './src/components/Icon/Icon';
 
 function App(): JSX.Element {
@@ -11,24 +12,33 @@ function App(): JSX.Element {
     <ThemeProvider theme={theme}>
       <SafeAreaView>
         <View style={{paddingHorizontal: 24}}>
-          <Text preset={'headingLarge'}>CoffeeStack</Text>
+          <Text preset="headingLarge" marginBottom="s8">
+            Olá
+          </Text>
+          <Text preset="paragraphLarge" marginBottom="s40">
+            Digite o seu e-mail e senha para entrar
+          </Text>
 
-          <Box flexDirection="row">
-            <Icon name="chevronRight" size={50} />
-            <Icon name="heartFill" color="buttonPrimary" />
-            <Icon name="profile" size={50} />
-            <Icon name="profileFill" size={50} />
-            <Icon name="heart" size={50} />
-            <Icon name="bellOn" color="carrotSecondary" size={50} />
-          </Box>
-          <Box flexDirection="row">
-            <Icon name="newPost" size={50} />
-            <Icon name="camera" size={50} />
-            <Icon name="chat" size={50} />
-            <Icon name="chatOn" color="error" size={50} />
-            <Icon name="flashOff" size={50} />
-            <Icon name="flashOn" size={50} />
-          </Box>
+          <TextInput
+            label="E-mail"
+            errorMessage="Mensagem de erro"
+            placeholder="Digite seu e-mail"
+            boxProps={{marginBottom: 's20'}}
+          />
+
+          <TextInput
+            label="Senha"
+            placeholder="Digite sua senha"
+            rightComponent={<Icon name="eyeOn" color="gray2" />}
+            boxProps={{marginBottom: 's10'}}
+          />
+
+          <Text preset="paragraphSmall" bold color="primary" marginBottom="s48">
+            Esqueci minha senha
+          </Text>
+
+          <Button title="Entrar" marginBottom="s12" />
+          <Button title="Criar uma conta" preset="outline" />
         </View>
       </SafeAreaView>
     </ThemeProvider>
