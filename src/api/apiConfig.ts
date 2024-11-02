b39b155fbec558aff3f8e079abe1503a2eebc6e2
André Andrade Santos <whoisandrade@gmail.com>
2024-11-01 23:58:51 -0300

    add comment post component and integration

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'http://127.0.0.1:3333/',
   headers: {
     Authorization:
-      'Bearer NA.etrD9hb9CQbQJ0EvuA-tM8jz1VLVQa9bceUnpcfp4snrBmc1-JB4qXFC2uMo',
+      'Bearer MQ.vhBAWblxBo1LsDGeJgYaAqZrxZON6oyYSQ6hySpGNg4VtogrdNocFgyBKPjJ',
   },
 });
